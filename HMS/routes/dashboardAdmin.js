cf7a525ff708935ce5826ed87900b3d68be12b55
smith-bimal <{golokbihareebeemal@gmail.com}>
2024-09-08 22:20:29 +0530

    Added pharmacy & Prescription Routes and controllers; Changed the isLoggedIn middleware to dynamicIsLoggedIn

--- HMS/routes/dashboardAdmin.js
+++ HMS/routes/dashboardAdmin.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const dashboard = require("../controller/dashboard");
-const { isAdminOrDoctor, isAuthenticated, isLoggedIn } = require('../utils/middlewares');
+const { isAdminOrDoctor, isAuthenticated, dynamicIsLoggedIn } = require('../utils/middlewares');
 
 router.get("/",
   isAdminOrDoctor,
-  isLoggedIn("admin"),
+  dynamicIsLoggedIn,
   isAuthenticated,
   dashboard.adminDashboard
 );

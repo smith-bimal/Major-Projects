856784cf9dd24e3bbf7d5da4233f80f367a2977a
smith-bimal <{golokbihareebeemal@gmail.com}>
2024-09-13 13:21:28 +0530

    Updated all listing details in init file

--- NookNest/public/js/customCalender.js
+++
@@ -1,9 +0,0 @@
-config = {
-    mode: "range",
-    minDate: "today",
-    dateFormat: "Y-m-d",
-    position: "below",
-}
-
-flatpickr("#search_date_1", config);
-flatpickr("#search_date_2", config);
